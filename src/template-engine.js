832c3fed599630ca7502858b450bebf5823157a2
alfthan <jalfthan@gmail.com>
2023-10-02 17:27:37 +0200

    adding support for or and and (#29)
    Co-authored-by: Jonas Alfthan <jonas@flaivy.com>

--- src/template-engine.js
+++ src/template-engine.js
@@ -148,6 +148,21 @@ handlebars.registerHelper("ifCond", function (v1, operator, v2, options) {
   }
 });
 
+Handlebars.registerHelper({
+  eq: (v1, v2) => v1 === v2,
+  ne: (v1, v2) => v1 !== v2,
+  lt: (v1, v2) => v1 < v2,
+  gt: (v1, v2) => v1 > v2,
+  lte: (v1, v2) => v1 <= v2,
+  gte: (v1, v2) => v1 >= v2,
+  and() {
+    return Array.prototype.every.call(arguments, Boolean);
+  },
+  or() {
+    return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
+  },
+});
+
 handlebars.registerHelper("eachUnique", function (array, options, context) {
   let itterator = array;
   if (lodash.isObject(array)) {
